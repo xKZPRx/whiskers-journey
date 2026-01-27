f54a1c1b7a431ef8b235806dbcc37ccd7d026f92
xKZPRx <kacper.wawrz09@gmail.com>
2026-01-27 18:40:33 +0100

    feat: calculate level width and height, assign solid booleans for tiles creation

--- core/level.py
+++ core/level.py
@@ -1,5 +1,5 @@
 from core.tile import Tile
-from settings import TILE_SIZE, WHITE_COLOR, YELLOW_COLOR
+from settings import TILE_SIZE, WHITE_COLOR, YELLOW_COLOR, BLUE_COLOR, BROWN_COLOR
 from core.player import Player
 
 
@@ -14,28 +14,37 @@ class Level:
         with open(f"levels/level{level_index}.txt", "r") as file:
             level_map = [line.strip() for line in file.readlines()]
 
+        max_x = 0
+        max_y = 0
+
         for y, line in enumerate(level_map):
             for x, char in enumerate(line.strip()):
-                self.width = x * TILE_SIZE
-                self.height = y * TILE_SIZE
+                px = x * TILE_SIZE
+                py = y * TILE_SIZE
+
+                max_x = max(max_x, px + TILE_SIZE)
+                max_y = max(max_y, py + TILE_SIZE)
 
                 match char:
                     case "1":
-                        self.tiles.append(Tile(x * TILE_SIZE, y * TILE_SIZE, WHITE_COLOR))
+                        self.tiles.append(Tile(x * TILE_SIZE, y * TILE_SIZE, WHITE_COLOR, True))
                     case "2":
-                        self.tiles.append(Tile(x * TILE_SIZE, y * TILE_SIZE, WHITE_COLOR))
+                        self.tiles.append(Tile(x * TILE_SIZE, y * TILE_SIZE, WHITE_COLOR, True))
                     case "P":
                         self.player = Player(x * TILE_SIZE, y * TILE_SIZE)
                     case "F":
-                        pass
+                        self.tiles.append(Tile(x * TILE_SIZE, y * TILE_SIZE, BLUE_COLOR, False))
                     case "K":
-                        self.tiles.append(Tile(x * TILE_SIZE, y * TILE_SIZE, YELLOW_COLOR))
+                        self.tiles.append(Tile(x * TILE_SIZE, y * TILE_SIZE, YELLOW_COLOR, False))
                     case "D":
-                        pass
+                        self.tiles.append(Tile(x * TILE_SIZE, y * TILE_SIZE, BROWN_COLOR, False))
                     case ".":
                         pass
                     case _:
                         print("No char match found.")
+
+        self.width = max_x
+        self.height = max_y
 
 
     def draw(self, screen, camera):
