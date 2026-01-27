9e8b5f468f1a6195a4b5352417c6aac1aa127cf2
xKZPRx <kacper.wawrz09@gmail.com>
2026-01-27 12:38:13 +0100

    feat: integrate Level and Player into Game loop

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from settings import WINDOW_WIDTH, WINDOW_HEIGHT, FPS, BLACK_COLOR
+from core.level import Level
+from core.player import Player
 
 
 class Game:
@@ -12,12 +14,14 @@ class Game:
         self.clock = pygame.time.Clock()
 
         self.running = True
+        self.level_index = 1
 
         self.load_resources()
 
 
     def load_resources(self):
-        pass
+        self.level = Level(self.level_index)
+        self.player = Player(100, 100)
 
 
     def handle_events(self):
@@ -29,9 +33,13 @@ class Game:
     def update(self):
         self.dt = self.clock.tick(FPS) / 1000
 
+        self.player.update(self.dt)
+
 
     def draw(self):
         self.screen.fill(BLACK_COLOR)
+        self.level.draw(self.screen)
+        self.player.draw(self.screen)
 
         pygame.display.flip()
 
